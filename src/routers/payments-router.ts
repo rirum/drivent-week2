7ace5ee15330b46e68c3c593ea8029a48b8d3693
camille <cerulean.dsgn@gmail.com>
2023-04-17 03:21:19 -0300

    feat: routes defined

---
+++ src/routers/payments-router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const paymentsRouter = Router();
+
+paymentsRouter.post('/');
+
+export { paymentsRouter };
---
+++ src/routers/tickets-router.ts
@@ -0,0 +1,7 @@
+import { Router } from 'express';
+
+const ticketsRouter = Router();
+
+ticketsRouter.post('/');
+
+export { ticketsRouter };
